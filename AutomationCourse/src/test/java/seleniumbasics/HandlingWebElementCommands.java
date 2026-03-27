96d56751f518e27650aa3a8f078be98d2991f208
LintaLukose <lintalukose2000gmail.com>
2026-03-26 18:11:54 -0700

    commit 2

--- AutomationCourse/src/test/java/seleniumbasics/HandlingWebElementCommands.java
+++ AutomationCourse/src/test/java/seleniumbasics/HandlingWebElementCommands.java
@@ -1,17 +1,32 @@
 package seleniumbasics;
 
+import java.time.Duration;
+import java.util.NoSuchElementException;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement; //web element is a predefined interface 
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HandlingWebElementCommands extends Base {
 	public void verifyWebElement() {
 		driver.navigate().to("https://selenium.qabible.in/simple-form-demo.php");
 		WebElement messgebox = driver.findElement(By.id("single-input-field")); // The name of the WebElement should be
 																				// similar to the element
-		messgebox.sendKeys("Test Message"); //to send a message 
+		messgebox.sendKeys("Test Message"); // to send a message
 		WebElement showmessagebutton = driver.findElement(By.id("button-one"));
 		System.out.println(showmessagebutton.isDisplayed()); // to check if it is displayed. returns a boolean value
 		System.out.println(showmessagebutton.isEnabled());
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+		wait.until(ExpectedConditions.elementToBeClickable(showmessagebutton));
+		// fluent wait
+		Wait<WebDriver> fluent = new FluentWait<WebDriver>(driver).withTimeout(Duration.ofSeconds(5))
+				.pollingEvery(Duration.ofSeconds(1)).ignoring(NoSuchElementException.class);
+		fluent.until(ExpectedConditions.elementToBeClickable(showmessagebutton));
+	
 		showmessagebutton.click();
 		WebElement yourmessage = driver.findElement(By.id("message-one"));
 		System.out.println(yourmessage.getText()); // to get the given message in console
